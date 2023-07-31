99d425b6b75fdd350d6bbbc12a00cc1b32ffe514
kror-shack <tariqfouzan1@gmail.com>
2023-07-31 17:05:23 +0300

    Add: TodaysDate Component

---
+++ src/components/TodaysDate/TodaysDate.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TodaysDate: React.FC = () => {
+  const today = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "2-digit",
+  };
+  const formattedDate: string = today.toLocaleDateString(undefined, options);
+
+  return <p>date:- {formattedDate}</p>;
+};
+
+export default TodaysDate;
