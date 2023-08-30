bd73663f25e5df5114b265c402fa4f43a12dcfce
kror-shack <tariqfouzan1@gmail.com>
2023-08-30 17:49:12 +0300

    Enable Jest to Handle SCSS and SVGs

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,14 @@ module.exports = {
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
     "^.+\\.(js|jsx)$": "babel-jest",
+    "^.+\\.svg$": "<rootDir>/svgTransform.js",
   },
+  moduleNameMapper: {
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(js|jsx|ts|tsx)$",
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx"],
 
   collectCoverage: true,
 };
