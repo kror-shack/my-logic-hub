b80a5cc5c8f7abb25cdefe47d3f6a1ca282637be
kror-shack <tariqfouzan1@gmail.com>
2023-10-03 15:22:20 +0300

    Update header styles
    this commit incoprates new media queries, and changes the visual layout of the header

--- src/components/TodaysDate/TodaysDate.tsx
+++ src/components/TodaysDate/TodaysDate.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TodaysDate.scss";
 
 /**
  * A React component that displays today's date in a dd/mm/yy.
  *
  * @component
- * @returns {JSX.Element} - A React JSX element displaying the formatted date.
+ * @returns- A React JSX element displaying the formatted date.
  */
 const TodaysDate = () => {
+  const [isWideScreen, setIsWideScreen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsWideScreen(window.innerWidth > 768);
+    };
+
+    checkScreenWidth(); // Initial check
+
+    const handleResize = () => {
+      checkScreenWidth();
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const today = new Date();
-  const options: Intl.DateTimeFormatOptions = {
-    day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
-  };
+  const options: Intl.DateTimeFormatOptions = isWideScreen
+    ? {
+        day: "2-digit",
+        month: "2-digit",
+        year: "2-digit",
+      }
+    : {
+        day: "2-digit",
+        month: "2-digit",
+      };
   const formattedDate: string = today.toLocaleDateString(undefined, options);
 
   return <p className="Date">{formattedDate}</p>;
